3ece5eac3c27f0cfff417f56c0a856219e80f0b9
samjaberi <samanjaberi.sj@gmail.com>
2023-01-18 18:26:19 -0500

    bug fix

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,7 +1,7 @@
 import { Configuration, OpenAIApi } from "openai";
 
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: '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',
 });
 const openai = new OpenAIApi(configuration);
 
